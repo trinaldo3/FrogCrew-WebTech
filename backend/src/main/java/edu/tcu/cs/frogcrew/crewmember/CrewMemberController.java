a6e10eed18e5b05f813fc5750b73d33bce05a55d
brian <brian.shrestha@tcu.edu>
2025-04-28 10:31:09 -0500

    Front end changes api calls

--- backend/src/main/java/edu/tcu/cs/frogcrew/crewmember/CrewMemberController.java
+++ backend/src/main/java/edu/tcu/cs/frogcrew/crewmember/CrewMemberController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/crewmember") //Change
 public class CrewMemberController {
--- frontend/src/components/CrewMemberRegistration.vue
+++ frontend/src/components/CrewMemberRegistration.vue
@@ -61,7 +61,7 @@ export default {
       if (!validate()) return;
       try {
         const res = await fetch(
-          `/crewmember/crewmember?token=${INVITE_TOKEN}`,
+          `http://localhost:8080/crewmember/crewmember?token=${INVITE_TOKEN}`,
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
